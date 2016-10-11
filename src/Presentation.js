0799319d005a14d266cf46b87868745c7db4269b
Marshall Bowers <elliott.codes@gmail.com>
2016-10-11 00:05:54 -0400

    Set up Prism styles

--- src/Presentation.js
+++ src/Presentation.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import { Spectacle, Deck, Slide } from 'spectacle';
 import createHistory from 'history/createBrowserHistory'
 import CodeSlide from 'spectacle-code-slide';
+import {
+  Spectacle,
+  Deck,
+  Slide
+} from 'spectacle';
+
 import theme from './theme';
 import './Presentation.css';
 
@@ -24,16 +29,19 @@ class Presentation extends Component {
               A Natural Progression Towards Declarative Programming
             </h2>
           </Slide>
-          <Slide>
-          </Slide>
           <CodeSlide
             lang='ocaml'
             code={require('raw!./examples/ocaml/fibonacci.ml')}
             ranges={[
               {
                 loc: [0, 3]
+              },
+              {
+                loc: [3, 4]
               }
-            ]} />
+            ]}
+            bgColor='inherit'
+            transition={[]} />
         </Deck>
       </Spectacle>
     );
